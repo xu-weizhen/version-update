dc8577b0b8c7a6895af4f919b141095527a08a23
Xu-weizhen <xxuweizhen@qq.com>
2021-11-08 19:52:03 +0800

    update

--- database.sql
+++ database.sql
@@ -1,3 +1,13 @@
+create database update_version character set utf8mb4;
+
+create user 'uv_query_usr'@'%' identified by '123456';
+grant select on update_version.* to 'uv_query_usr'@'%';
+
+create user 'uv_admin_usr'@'%' identified by '123456';
+grant all on update_version.* to 'uv_admin_usr'@'%';
+
+use update_version;
+
 -- 创建安卓的版本规则包
 CREATE TABLE rulesForAndroid(
 	`aid` INT,
@@ -14,7 +24,7 @@ CREATE TABLE rulesForAndroid(
 	`title` varchar(127) not null,
 	`update_tips` varchar(255) not null,
 	PRIMARY KEY(`aid`,`platform`,`update_version_code` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 创建iOS的版本规则包
 CREATE TABLE rulesForiOS(
@@ -30,7 +40,7 @@ CREATE TABLE rulesForiOS(
 	`title` varchar(127) not null,
 	`update_tips` varchar(255) not null,
 	PRIMARY KEY(`aid`,`platform`,`update_version_code` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 统计下载次数
 CREATE TABLE downloadCount(
@@ -39,7 +49,7 @@ CREATE TABLE downloadCount(
 	`update_version_code` varchar(40),
 	`count` BIGINT,
 	PRIMARY KEY(`aid`,`platform`,`update_version_code` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 
